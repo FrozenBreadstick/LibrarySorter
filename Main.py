f3d28201791dbdfe6dd82148686d1e0e173a23de
Lumdrix <plumlucas@hotmail.co.uk>
2024-09-20 22:48:29 +1000

    Created main code file

---
+++ Main.py
@@ -0,0 +1,21 @@
+#Imports
+from ir_support import DHRobot3D #Industrial Robotics Specific
+import swift #Rendering for the robots in browser
+from spatialmath import * #matrix math
+from spatialmath.base import *
+import spatialgeometry as geometry
+import roboticstoolbox as rtb #robotics toolbox used for various calculations
+from roboticstoolbox import *
+import numpy as np #Additional math operations
+import os
+import time
+import logging
+import sys
+
+def main():
+    """
+    Main Robot code
+    """
+
+if __name__ == "__main__":
+    main()
