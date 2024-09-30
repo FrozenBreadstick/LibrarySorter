a6e895d5987f38b5943bda7f7d8df238219882a1
qbnUbun <quoc.b.nguyen.work@gmail.com>
2024-09-30 23:42:34 +1000

    Fix sorter

--- Book_sort_lib/Sorting_function.py
+++ Book_sort_lib/Sorting_function.py
@@ -1,13 +1,5 @@
 import numpy
 
-def run(availableSpace):
-
-    # Check to ensure input is a positive integer
-    if not isinstance(availableSpace, int) or not availableSpace < 0:#NEED FIXXXXXXX
-        raise ValueError("availableSpace must be a positive integer")
-    
-    aavailableSpace_=availableSpace #Work out away to get this value be scaned by the main robot 
-
 def find_combinations(n, books, current_combination, all_combinations):
     # Base case: If we filled exactly the space
     if n == 0:
