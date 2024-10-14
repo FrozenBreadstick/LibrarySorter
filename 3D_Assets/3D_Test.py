d10ecf65da42d2a18b45834781d7ecc296f73f40
FrozenBreadstick <mattia.sydcanfam@gmail.com>
2024-10-14 17:53:16 +1100

    Added Blender Model File Variants (New Design + Fixed Normals) 3D_Test File contains basic functioning code for the LibSort Robot (End wrist is curently broken, working on fix)

--- 3D_Assets/3D_Test.py
+++ 3D_Assets/3D_Test.py
@@ -2,6 +2,8 @@ import swift
 import os
 import time
 from roboticstoolbox import *
+import roboticstoolbox as rtb
+import spatialgeometry as geometry
 from ir_support.robots.DHRobot3D import DHRobot3D
 import spatialmath.base as spb
 from spatialmath import SE3
@@ -16,14 +18,14 @@ class LibraryBot(DHRobot3D):
         links = self._create_DH()     
 
         #Create difctionary of model names
-        link3D_names = dict(link0 = 'base_rail',
-                            link1 = 'secondary_rail',
-                            link2 = 'gantry_1', 
-                            link3 = 'elbow_1',
-                            link4 = 'elbow_2',
-                            link5 = 'elbow_3',
-                            link6 = 'elbow_4',
-                            link7 = 'wrist_1')
+        link3D_names = dict(link0 = 'base_rail_b',
+                            link1 = 'secondary_rail_b',
+                            link2 = 'gantry_1_b', 
+                            link3 = 'elbow_1_b',
+                            link4 = 'elbow_2_b',
+                            link5 = 'elbow_3_b',
+                            link6 = 'elbow_4_b',
+                            link7 = 'wrist_1_b')
 
         #Set default transforms of models
         qtest = [0, 0, 0, 0, 0, 0, 0]
@@ -40,7 +42,8 @@ class LibraryBot(DHRobot3D):
         current_path = os.path.abspath(os.path.dirname(__file__))
         super().__init__(links, link3D_names, name = 'LibraryBot', link3d_dir = current_path, qtest = qtest, qtest_transforms = qtest_transforms)
         
-        #Rotate the base to be in the correct orientation for the workspace\
+        #Rotate the base to be in the correct orientation for the workspace
+        self.base = self.base * SE3.Ry(pi/2) * SE3(-0.15,0,0)
         self.q = qtest
 # -----------------------------------------------------------------------------------#    
     def _create_DH(self):
@@ -48,15 +51,15 @@ class LibraryBot(DHRobot3D):
         Create robot's standard DH model
         """
         links = []
-        links.append(PrismaticMDH(theta = pi, a = 0, alpha = pi/2, qlim = [-1,1]))
-        links.append(PrismaticMDH(theta = 100, a = 100, alpha = 100, offset = 100, qlim = [-1,1]))
-        a = [0, 0, 0, 0, 0]
+        links.append(rtb.PrismaticDH(theta = pi/2, a = 0, alpha = pi/2, qlim = [-1,1]))
+        links.append(rtb.PrismaticDH(theta = 0, a = 0.167391, alpha = 0, qlim = [-1,1]))
+        a = [0.5284043, 0.397998, 0.397998, 0.307999, 0]
         d = [0, 0, 0, 0, 0]
-        alpha = [0, 0, 0, 0, 0] #Create the base rotational offset
-        qlim = [[-2*pi, 2*pi] for _ in range(6)] #Create the base joint limits for the robot
+        o = [0, 0, 0, 0, 0]
+        alpha = [0, pi/2, 0, pi/2, 0]
+        qlim = [[-2*pi, 2*pi] for _ in range(6)]
         for i in range(5):
-            link = RevoluteMDH(d=d[i], a=a[i], alpha=alpha[i], qlim= qlim[i]) #Create the revolving joints and parse them to a list for the super initialiser
-            links.append(link)
+            link = rtb.RevoluteDH(d=d[i], a=a[i], alpha=alpha[i], qlim= qlim[i], offset= o[i])
         return links
     
     def test(self):
@@ -64,7 +67,7 @@ class LibraryBot(DHRobot3D):
         env.launch(realtime= True)
         self.add_to_env(env)
         q1 = [0, 0, 0, 0, 0, 0, 0]
-        q2 = [1, 0, 0, 0, 0, 0, 0]
+        q2 = [1, -1, pi/2, 0, 0, 0, 0]
         qtraj = jtraj(q1, q2, 50).q
         for q in qtraj:
             self.q = q
@@ -77,4 +80,6 @@ class LibraryBot(DHRobot3D):
 
 if __name__ == "__main__":
     r = LibraryBot()
-    r.test()
+    # r.test()
+    fig = r.plot(r.q)
+    fig.hold()
