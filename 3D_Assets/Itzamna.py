cce546514633d8fb4df823f618e76ee0400093e5
FrozenBreadstick <mattia.sydcanfam@gmail.com>
2024-10-16 09:27:01 +1100

    Added "Itzamna.py" the main robot file. Updated blender file and renamed 3D_test to 3dim_test due to import errors.

---
+++ 3D_Assets/Itzamna.py
@@ -0,0 +1,104 @@
+import swift
+import os
+import time
+from roboticstoolbox import *
+import roboticstoolbox as rtb
+from ir_support.robots.DHRobot3D import DHRobot3D
+import spatialmath.base as spb
+from spatialmath import SE3
+from math import pi
+
+class Itzamna(DHRobot3D):
+    def __init__(self):    
+        """ 
+        Robot for sorting library books
+        """       
+        # DH links
+        links = self._create_DH()     
+
+        #Create difctionary of model names
+        link3D_names = dict(link0 = 'base_rail_b',
+                            link1 = 'secondary_rail_b',
+                            link2 = 'gantry_1_b', 
+                            link3 = 'elbow_1_b',
+                            link4 = 'elbow_2_b',
+                            link5 = 'elbow_3_b',
+                            link6 = 'elbow_4_b',
+                            link7 = 'wrist_1_b')
+
+        #Set default transforms of models
+        qtest = [0, 0, 0, 0, 0, 0, 0]
+        qtest_transforms = [spb.transl(0, 0, 0),
+                            spb.transl(0, 0, 0),
+                            spb.transl(0, 0, 0),
+                            spb.transl(0, 0, 0),
+                            spb.transl(0, 0, 0),
+                            spb.transl(0, 0, 0),
+                            spb.transl(0, 0, 0),
+                            spb.transl(0, 0, 0)] #All zero because blender models were exported with correct global transforms
+        
+        #Preset some base variables BEFORE passing the necessary items to the superclass initialisation to avoid them being destroyed
+        current_path = os.path.abspath(os.path.dirname(__file__))
+        super().__init__(links, link3D_names, name = 'Itzamna', link3d_dir = current_path, qtest = qtest, qtest_transforms = qtest_transforms)
+        
+        #Rotate the base to be in the correct orientation for the workspace
+        self.base = self.base * SE3.Ry(pi/2) * SE3(-0.15,0,0)
+        self.q = qtest
+# -----------------------------------------------------------------------------------#    
+    def _create_DH(self):
+        """
+        Create robot's standard DH model
+        """
+        links = []
+        links.append(rtb.PrismaticDH(theta = pi/2, a = 0, alpha = pi/2, qlim = [-1,1]))
+        links.append(rtb.PrismaticDH(theta = 0, a = 0.167391, alpha = 0, qlim = [-1,1]))
+        a = [0.5284043, 0.397998, 0.397998, 0, 0]
+        d = [0, 0, 0, 0, 0.307999]
+        o = [0, 0, 0, pi/2, -pi/2]
+        alpha = [0, pi/2, 0, pi/2, pi/2]
+        qlim = [[-2*pi, 2*pi] for _ in range(6)]
+        for i in range(5):
+            link = rtb.RevoluteDH(d=d[i], a=a[i], alpha=alpha[i], qlim= qlim[i], offset= o[i])
+            links.append(link)
+        return links
+    
+    def _create_Elipsoid_Collision_Profile(self):
+        pass #Function that will outline ellipsoid collision for robot models
+    
+    def test(self):
+        env = swift.Swift()
+        env.launch(realtime= True)
+        self.add_to_env(env)
+        q1 = [0, 0, 0, 0, 0, 0, 0]
+        q2 = [1, -1, -pi/2, pi/2, -pi/2, pi/2, pi]
+        qtraj = jtraj(q1, q2, 50).q
+        for q in qtraj:
+            self.q = q
+            env.step()
+            time.sleep(0.02)
+        env.hold()
+
+    def add_to_env(self, env):
+        super().add_to_env(env)
+
+    def goto(self, pos, precision, threadnum, steps, accuracy):
+        """
+        Sends the robot to the given position avoiding any objects in it's way by implementing an A* algorithm
+        _____________________________________________________________________________________________________________
+        \npos: Position to send robot too.
+        \nprecision: Precision to use for A* algorithm, default is .1 (for lower values use a higher number of threads)
+        \nthreadnum: Maximum number of threads that can be used for collision checking during A* algorithm
+        \nsteps: Number of steps to take between each node
+        \naccuracy: Number of IK solutions to calculate before deciding on lowest cost
+        
+        """
+        pass #Function that will implement A* pathfinding with an elispoid collision check
+
+    def iscollided(self, object):
+        pass #Function that will check if 
+
+if __name__ == "__main__":
+    r = Itzamna()
+    r.test()
+    # fig = r.plot(r.q)
+    # fig.hold()
