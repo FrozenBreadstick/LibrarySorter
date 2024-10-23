2a0d89080b5697cd27048c59842034bac793dfb6
Lumdrix <plumlucas@hotmail.co.uk>
2024-10-23 20:49:46 +1100

    made the movement slightly more stable

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -42,7 +42,7 @@ class GUI():
         self.ActiveBot.q = self.ActiveBot.q + dq
     
     def WidgetUpdate(self):
-        self.ActiveBotLabel.desc = ("Active Robot: " + str(self.ActiveBot.name))
+        self.ChangeBotButton.desc = (str(self.ActiveBot.name))
         #Positions
         self.EEPosx.desc = ("X: " + str(np.round(self.ActiveBot.fkine(self.ActiveBot.q).x, 2))) 
         self.EEPosy.desc = ("Y: " + str(np.round(self.ActiveBot.fkine(self.ActiveBot.q).y, 2)))
@@ -50,13 +50,13 @@ class GUI():
         #Sliders
         j = 0
         for l in self.Sliders: #Change Slider Limits
-            self.Sliders["Link{0}".format(j+1)].cb = lambda x, j=j: self.set_joint(j, x)
-            self.Sliders["Link{0}".format(j+1)].value = np.round(np.rad2deg(self.ActiveBot.q[j]), 2)
+            if self.Sliders["Link{0}".format(j+1)].value != np.round(np.rad2deg(self.ActiveBot.q[j]), 2):
+                self.Sliders["Link{0}".format(j+1)].value = np.round(np.rad2deg(self.ActiveBot.q[j]), 2)
             j += 1
 
     def set_joint(self, j, value): #Sets the joint angle
-        # self.ActiveBot.q[j] = np.deg2rad(float(value)) #When updating value on slider this gets run causing jittering
-        print(value)
+        self.ActiveBot.q[j] = np.deg2rad(float(value)) #When updating value on slider this gets run causing jittering
+        print(j)
 
     def ChangeBot(self): #Changes which robot is currently being controlled
         if self.ActiveBot == self.Itz:
@@ -71,12 +71,11 @@ class GUI():
                 j += 1
 
     def CreateWidgets(self, env): #Add buttons, text and sliders to the swift environment
-        self.ActiveBotLabel = swift.Label("Active Robot: " + str(self.ActiveBot.name))
-        env.add(swift.Button(lambda x : self.ChangeBot(), 'Change Bot'))
+        self.ChangeBotButton = swift.Button(lambda x : self.ChangeBot(), str(self.ActiveBot.name))
         self.EEPosx = swift.Label("X: " + str(np.round(self.ActiveBot.fkine(self.ActiveBot.q).x, 2))) 
         self.EEPosy = swift.Label("Y: " + str(np.round(self.ActiveBot.fkine(self.ActiveBot.q).y, 2)))
         self.EEPosz = swift.Label("Z: " + str(np.round(self.ActiveBot.fkine(self.ActiveBot.q).z, 2)))
-        env.add(self.ActiveBotLabel)
+        env.add(self.ChangeBotButton)
         env.add(self.EEPosx)
         env.add(self.EEPosy)
         env.add(self.EEPosz)
@@ -93,7 +92,7 @@ class GUI():
                                                             min=np.round(np.rad2deg(l.qlim[0]), 2),
                                                             max=np.round(np.rad2deg(l.qlim[1]), 2),
                                                             step=1,
-                                                            value=np.round(np.rad2deg(self.Itz.q[j]), 2),
+                                                            value=np.round(np.rad2deg(self.ActiveBot.q[j]), 2),
                                                             desc="Joint " + str(j),
                                                             unit="&#176;",))
                 env.add(self.Sliders["Link{0}".format(str(j+1))])
@@ -115,7 +114,7 @@ class GUI():
                 self.vz -= 0.1
 
     def ControllerJog(self, env): #Jogs the robot with the Xbox controller
-        kv = 0.3
+        kv = 0.7
         self.vx = kv * Controller.XboxController.read(self.Control)[0]/10 #X-Axis control
         self.vz = kv * Controller.XboxController.read(self.Control)[1]/10 #Z-Axis control
         self.vy = kv * (Controller.XboxController.read(self.Control)[3]/10 - Controller.XboxController.read(self.Control)[2]/10) #Y-Axis Control
