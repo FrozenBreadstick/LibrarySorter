d0cf5596b47d588342d9843cd7ff82342782e65c
qbnUbun <quoc.b.nguyen.work@gmail.com>
2024-10-26 22:29:48 +1100

    Error with setting up the scene

--- Main.py
+++ Main.py
@@ -13,20 +13,16 @@ import time
 import logging
 import sys
 import threading
-<<<<<<< HEAD
 #custom other files
 from UR3E import UR3E
 from GUI import GUI
 from Models import Itzamna, libBot #Import the 3D model of the robot
+from math import pi
 
 env = swift.Swift()
 env.launch(realtime=True)
 
-UR3 = UR3E.UR3E()
-Itz = Itzamna.Itzamna()
 
-UR3.add_to_env(env)
-Itz.add_to_env(env)
 
 t = geometry.Cuboid([0.5,0.5,0.5], collision=True, pose=SE3(0.8,0.8,0.8))
 b= geometry.Cuboid([0.5,0.5,0.5], collision=True)
@@ -35,41 +31,17 @@ b= geometry.Cuboid([0.5,0.5,0.5], collision=True)
 env.add(t)
 env.add(b)
 
-ControlPanel = GUI.GUI(env, UR3, Itz)
+#ControlPanel = GUI.GUI(env, UR3, Itz)
 
 def main():
-    Itz.q = ControlPanel.Itz.q
-    UR3.q = ControlPanel.UR3.q
-    b.T = Itz.fkine(Itz.q)
-    print(test(b, t))
-    env.step()
-=======
-from libBot_Assets import libBot #Import the 3D model of the robot
-from UR3E import UR3E #Import the 3D model of the robot
-
-
-def EnvironmentSetup(): 
-     env=swift.Swift() #Create an environment object
-     env.launch() #Launch the environment
-        
-     rLibot = libBot.LibraryBot() #Create primary robot
-     uR= UR3E.UR3E() #Create secondary robot
-     uR.base = uR.base@SE3(0.5,0,1) #Move the secondary robot to the side
-     print(uR.base)
-     rLibot.add_to_env(env)
-     uR.add_to_env(env)
-              
-     env.hold() #Hold the environment
-     
-
-     
-     
-
-def mainCode():
-    EnvironmentSetup()
-  
+    EnvironmentSetup(env)
     
->>>>>>>  Resolve merge conflict
+    """ b.T = Itz.fkine(Itz.q)
+    print(test(b, t)) ## not working
+     """
+    env.step()
+    
+   # env.hold()
     pass
 
 def test(robot, shape):
@@ -80,31 +52,55 @@ def test(robot, shape):
         return False
 
 #-----------------------------------Main----------------------------#
-def EnvironmentSetup(): 
-     rLibot = DHRobot3D.LibraryBot()
+def EnvironmentSetup(env): 
+   
+    
+    
+    #Create the robots
+    Itz = Itzamna.Itzamna()
+    UR3 = UR3E.UR3E()    
+
+    #Add the robots to the environment
+    Itz.add_to_env(env)
+    UR3.add_to_env(env)
+    
+    #add the bookshelf to the environment
+    bookCase_path='LibrarySorter/temp/bookcase.dae'
+    if not os.path.exists(bookCase_path):
+        raise FileNotFoundError(f"Mesh file not found: {bookCase_path}")
+    
+    bookCaseMesh=geometry.Mesh(filename=bookCase_path,pose=SE3(0.8,0.8,0.8),color=(0.4,0.04,0.04))
+    env.add(bookCaseMesh) 
+    
+    print("Setting up environment")
+    
+    
     
 def check_Stop_press():
     #This function will check for a key press
     #Connect to read pin off arduino
+    
+    
     pass
 
 def run():
     #This part do threads to check for a key press
     t1 = threading.Thread(target=check_Stop_press)
-<<<<<<< HEAD
+    
     t2=threading.Thread(target=main)
     
-=======
-    t2=threading.Thread(target=mainCode)
-    t1.start()
+    t1.start()  
     t2.start()
+    
     t1.join()
     t2.join()
->>>>>>>  Resolve merge conflict
+
+def run2():
+    main()
 
 if __name__ == "__main__":
     env.set_camera_pose([1.3,-2.3,1.3], [1.3,0,1.3])
-    while True:
-        run()
-        main()
-        ControlPanel.Refresh()
+    run2()
+    
+    """ while True:
+        ControlPanel.Refresh() """
